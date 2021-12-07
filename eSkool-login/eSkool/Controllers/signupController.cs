92a2d4e97e016c31b12dae81d865b72b3b4fefd0
Eskool18 <eskooleims@gmail.com>
2021-12-07 18:52:26 +0500

    initial commit

---
+++ eSkool-login/eSkool/Controllers/signupController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eSkool.Controllers
+{
+    public class signupController : Controller
+    {
+        public IActionResult signUp()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ eSkool-login/eSkool/Models/UserInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace eSkool.Models
+{
+    public partial class UserInfo
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string UserName { get; set; } = null!;
+        [Required]
+        public string Password { get; set; } = null!;
+        public string Role { get; set; } = null!;
+    }
+}
