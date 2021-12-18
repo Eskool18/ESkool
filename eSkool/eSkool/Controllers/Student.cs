20651045b7315ffe0d101eb08ec8b96107e6f334
Maira509 <bsef18a509@pucit.edu.pk>
2021-12-19 01:57:17 +0500

    Implemented Views in Student Portal: -Implemented Attendance view -Implemented Books Download View -Added links to navbar

--- eSkool/eSkool/Controllers/Student.cs
+++ eSkool/eSkool/Controllers/Student.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
+using Grpc.Core;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eSkool.Controllers
@@ -16,5 +18,36 @@ namespace eSkool.Controllers
         {
             return View();
         }
+        public ActionResult subjects()
+        {
+            return View();
+        }
+        public ActionResult challan()
+        {
+            return View();
+        }
+        public ActionResult attendance()
+        {
+            return View();
+        }
+        public ActionResult download(string filePath)
+        {
+            string fullName = "wwwroot" + filePath;
+            //string fullName = Server.MapPath("~" + filePath);
+            byte[] fileBytes = GetFile(fullName);
+            return File(
+                fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, filePath);
+        }
+       
+        byte[] GetFile(string s)
+        {
+            System.IO.FileStream fs = System.IO.File.OpenRead(s);
+            byte[] data = new byte[fs.Length];
+            int br = fs.Read(data, 0, data.Length);
+            if (br != fs.Length)
+                throw new System.IO.IOException(s);
+            return data;
+        }
+
     }
 }
